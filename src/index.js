84087a060ccd80e6b0a9011749c97daf27589536
Bahdan Pryimachuk <bahdanchik@tut.by>
2020-10-01 18:14:35 +0300

    feat: Implement human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function toReadable (number) {
+    let res = '';
+  const units = ['zero', 'one', 'two', 'three', 'four', 'five', 
+                 'six', 'seven', 'eight', 'nine'];
+  const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen',
+                 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const dozens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 
+                  'sixty', 'seventy', 'eighty', 'ninety'];
+
+  if (Math.floor(number / 100) !== 0) {
+    res += `${units[Math.floor(number / 100)]} hundred `;
+    if(number % 100 === 0){
+        return res.trim();
+      } else {
+        number %= 100;
+      }
+  }
+
+  if (Math.floor(number / 10) !== 0) {
+    if (Math.floor(number / 10) === 1) {
+      res += `${teens[Math.floor(number % 10)]} `;
+      return res.trim();
+    } else {
+      res += `${dozens[Math.floor(number / 10)]} `;
+    }
+    if( number % 10 === 0){
+      return res.trim();
+    } else {
+      number %= 10;
+    }
+  }
   
+  if (number === 0 && res.length === 0) {
+    res = units[0];
+  } else {
+    res += units[number];
+  }
+  return res.trim();
 }
